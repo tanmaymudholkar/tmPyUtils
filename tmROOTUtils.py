8398a9f6e6ac2afd6b419c875a4bcce3fd1fab58
Tanmay Mudholkar <tkmudholkar@gmail.com>
2018-05-01 19:29:15 +0200

    Added option to fine-tune custom printing of text on top of bins for 2D histograms.

--- tmROOTUtils.py
+++ tmROOTUtils.py
@@ -113,11 +113,12 @@ def extractTH2Contents(inputTH2, outputFileName, columnTitles=None, quantityName
     outputFile.close()
     print("Histogram contents saved to file %s"%(outputFileName))
 
-def plotObjectsOnCanvas(listOfObjects=None, canvasName="", outputROOTFile=None, outputDocumentName="", outputDocumentExtension="png", canvas_xPixels=1024, canvas_yPixels=768, customOptStat=None, customPlotOptions_firstObject="", enableLogX = False, enableLogY = False, enableLogZ = False):
+def plotObjectsOnCanvas(listOfObjects=None, canvasName="", outputROOTFile=None, outputDocumentName="", outputDocumentExtension="png", canvas_xPixels=1024, canvas_yPixels=768, customOptStat=None, customTextFormat=None, customPlotOptions_firstObject="", enableLogX = False, enableLogY = False, enableLogZ = False):
     if (listOfObjects is None): sys.exit("Error in plotObjectsOnCanvas: no object found in listOfObjects.")
     if (canvasName == ""): sys.exit("Error in plotObjectsOnCanvas: No name specified for canvas.")
     if ((outputROOTFile is None) and (outputDocumentName == "")): sys.exit("Error in plotObjectsOnCanvas: Neither output ROOT file nor output document name specified.")
     if not(customOptStat is None): ROOT.gStyle.SetOptStat(customOptStat)
+    if not(customTextFormat is None): ROOT.gStyle.SetPaintTextFormat(customTextFormat)
     canvas = ROOT.TCanvas(canvasName, canvasName, canvas_xPixels, canvas_yPixels)
     canvas.SetBorderSize(0)
     canvas.SetFrameBorderMode(0)
