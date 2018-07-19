464f970bf7cf04d047626c6968c8f28a38de0787
Tanmay Mudholkar <tkmudholkar@gmail.com>
2018-07-19 09:55:10 +0200

    Added utilities to write a series of variables to a text file in the format (type name = value) and to fetch those variables from a file in this format.

--- tmGeneralUtils.py
+++ tmGeneralUtils.py
@@ -1,5 +1,7 @@
 from __future__ import print_function, division
 
+import sys
+
 def prettyPrintDictionary(inputDict, valueFormatSpecification="", keyPrintOrder=[]):
     stringLengths = [len(str(key)) for key in inputDict.keys()]
     maxStringLength = max(stringLengths)
@@ -12,3 +14,35 @@ def prettyPrintDictionary(inputDict, valueFormatSpecification="", keyPrintOrder=
         else:
             printStatement += "{dictionaryContent:" + valueFormatSpecification + "}"
         print(printStatement.format(keyName=key, dictionaryContent=inputDict[key]))
+
+def getConfigurationFromFile(inputFilePath):
+    configuration = {}
+    configurationFileObject = open(inputFilePath, "r")
+    for line in configurationFileObject:
+        splitLine = line.split()
+        parameterType = splitLine[0]
+        whitespaceRemovedNameValueString = "".join(splitLine[1:])
+        nameValuePair = whitespaceRemovedNameValueString.split("=")
+        if not(len(nameValuePair) == 2): sys.exit("ERROR: unable to parse the following as \"type arg=value\": {line}, or, split: {sL}. Attempted parameter type: {pT}, name-value pair string: {nVPS}, name-value pair: {nVP}".format(line=line, sL=splitLine, pT=parameterType, nVPS=whitespaceRemovedNameValueString, nVP=str(nameValuePair)))
+        if parameterType == "int":
+            value = int(nameValuePair[1])
+        elif parameterType == "float":
+            value = float(nameValuePair[1])
+        elif parameterType == "string":
+            value = nameValuePair[1]
+        else:
+            sys.exit("Unrecognized parameter type: {pT}".format(pT=parameterType))
+        configuration[nameValuePair[0]] = value
+    configurationFileObject.close()
+    return configuration
+
+def writeConfigurationParametersToFile(configurationParametersList, outputFilePath):
+    outputFileObject = open(outputFilePath, "w")
+    for configurationParameters in configurationParametersList:
+        if not(len(configurationParameters) == 3): sys.exit("ERROR: configuration parameters {cP} not in format (type, name, value)".format(cP=str(configurationParameters)))
+        parameterType = configurationParameters[0]
+        if not(parameterType in ["int", "float", "string"]): sys.exit("ERROR: Unrecognized parameter type from configuration parameters: {cP}, containing type: {t}".format(cP=str(configurationParameters), t=parameterType))
+        parameterName = configurationParameters[1]
+        parameterValue = configurationParameters[2]
+        outputFileObject.write("{t} {name}={value}\n".format(t=parameterType, name=parameterName, value=parameterValue))
+    outputFileObject.close()
