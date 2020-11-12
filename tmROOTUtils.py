0f2aae9124211d46f05e147f899ef3272b65d927
Tanmay Mudholkar <tkmudholkar@gmail.com>
2020-11-12 00:41:27 -0600

    tmROOTUtils.py: changed default save format from png to pdf.

--- tmROOTUtils.py
+++ tmROOTUtils.py
@@ -219,7 +219,7 @@ def extractTH2Contents(inputTH2, outputFileName, columnTitles=None, quantityName
     outputFile.close()
     print("Histogram contents saved to file %s"%(outputFileName))
 
-def plotObjectsOnCanvas(listOfObjects=None, canvasName="", outputROOTFile=None, outputDocumentName="", outputDocumentExtension="png", canvas_xPixels=1024, canvas_yPixels=768, customOptStat=None, customTextFormat=None, customPlotOptions_firstObject="", enableLogX = False, enableLogY = False, enableLogZ = False, customXRange=None, customYRange=None, customZRange=None):
+def plotObjectsOnCanvas(listOfObjects=None, canvasName="", outputROOTFile=None, outputDocumentName="", outputDocumentExtension="pdf", canvas_xPixels=1024, canvas_yPixels=768, customOptStat=None, customTextFormat=None, customPlotOptions_firstObject="", enableLogX = False, enableLogY = False, enableLogZ = False, customXRange=None, customYRange=None, customZRange=None):
     if (listOfObjects is None): sys.exit("Error in plotObjectsOnCanvas: no object found in listOfObjects.")
     if (canvasName == ""): sys.exit("Error in plotObjectsOnCanvas: No name specified for canvas.")
     if ((outputROOTFile is None) and (outputDocumentName == "")): sys.exit("Error in plotObjectsOnCanvas: Neither output ROOT file nor output document name specified.")
