15ff0ec5bf02f6c9c0f0c1cb1a98dd6cdbad60dd
Tanmay Mudholkar <tkmudholkar@gmail.com>
2021-09-16 08:41:37 -0500

    tmDASUtils: added function to print number of events alongside datasets.

--- tmDASUtils.py
+++ tmDASUtils.py
@@ -2,6 +2,17 @@ from __future__ import print_function, division
 
 import os, sys, subprocess
 
+def get_number_of_events_in_dataset(dataset_name):
+    dasgoclient_output_raw = str(subprocess.check_output("dasgoclient -query \"file dataset={d} | grep file.nevents\"".format(d=dataset_name), shell=True, universal_newlines=True, executable="/bin/bash"))
+    total_nevents = 0
+    for nevents_line in dasgoclient_output_raw.splitlines():
+        nevents = int(nevents_line)
+        total_nevents += nevents
+    return total_nevents
+
+def print_number_of_events_in_dataset(dataset_name):
+    print("#evts in {d}: {n}".format(d=dataset_name, n=get_number_of_events_in_dataset(dataset_name)))
+
 def get_list_of_datasets(query_string):
     dasgoclient_output_raw = str(subprocess.check_output("dasgoclient -query \"dataset dataset={q}\"".format(q=query_string), shell=True, universal_newlines=True, executable="/bin/bash"))
     datasets_list = []
@@ -13,13 +24,6 @@ def print_list_of_datasets(query_string):
     for dataset in get_list_of_datasets(query_string):
         print(dataset)
 
-def get_number_of_events_in_dataset(dataset_name):
-    dasgoclient_output_raw = str(subprocess.check_output("dasgoclient -query \"file dataset={d} | grep file.nevents\"".format(d=dataset_name), shell=True, universal_newlines=True, executable="/bin/bash"))
-    total_nevents = 0
-    for nevents_line in dasgoclient_output_raw.splitlines():
-        nevents = int(nevents_line)
-        total_nevents += nevents
-    return total_nevents
-
-def print_number_of_events_in_dataset(dataset_name):
-    print("#evts in {d}: {n}".format(d=dataset_name, n=get_number_of_events_in_dataset(dataset_name)))
+def print_list_of_datasets_with_nevts(query_string):
+    for dataset in get_list_of_datasets(query_string):
+        print("{d}: {n}".format(d=dataset, n=get_number_of_events_in_dataset(dataset)))
