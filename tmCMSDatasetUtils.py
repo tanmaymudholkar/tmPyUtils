94c2268e6a595dfaeb8bc541628da46f53116c6f
Tanmay Mudholkar <tkmudholkar@gmail.com>
2021-09-18 21:47:48 +0200

    tmCMSDatasetUtils.py: Functions that store cross-section info now work correctly, also a few more convenience functions.

--- tmCMSDatasetUtils.py
+++ tmCMSDatasetUtils.py
@@ -1,6 +1,8 @@
 from __future__ import print_function, division
 
-import os, sys, subprocess
+import os, sys, subprocess, re, math, json
+
+XSEC_FRACTIONAL_ERROR_TOLERANCE = 0.01
 
 def get_number_of_events_in_dataset(dataset_name):
     dasgoclient_output_raw = str(subprocess.check_output("dasgoclient -query \"file dataset={d} | grep file.nevents\"".format(d=dataset_name), shell=True, universal_newlines=True, executable="/bin/bash"))
@@ -33,22 +35,74 @@ def get_mcm_prepid_for_dataset(dataset_name):
     if (len(dasgoclient_output_raw.splitlines()) != 1): sys.exit("ERROR: multiple prepIDs for pattern: {p}".format(p=dataset_name))
     return str((dasgoclient_output_raw.splitlines())[0])
 
-def xsec_setup(shell_session_handle):
-    path_xsec_cmssw = os.environ["PATH_XSEC_CMSSW"]
-    xsec_scram_arch = os.environ["XSEC_SCRAM_ARCH"]
-    path_xsec_scripts = os.environ["PATH_XSEC_SCRIPTS"]
-    stdout_output, stderr_output = shell_session_handle.communicate("export SCRAM_ARCH={sa}".format(sa=xsec_scram_arch))
-    stdout_output, stderr_output = shell_session_handle.communicate("echo SCRAM_ARCH=${SCRAM_ARCH}")
-    print("stdout output of echo ${{SCRAM_ARCH}}: {o}".format(o=str(stdout_output)))
-    stdout_output, stderr_output = shell_session_handle.communicate("cd {d}".format(d=path_xsec_cmssw))
-    stdout_output, stderr_output = shell_session_handle.communicate("ls".format(sa=xsec_scram_arch))
-    print("stdout output of ls: {o}".format(o=str(stdout_output)))
+def parse_xsec_info_output(xsec_info_output, printDebug=False):
+    if printDebug: print("Starting to parse xsec process output...")
+    xsec_re = re.compile(r"^After filter: final cross section = ([^ ]*) \+- ([^ ]*) pb$")
+    eq_lumi_re = re.compile(r"^After filter: final equivalent lumi for 1M events \(1/fb\) = ([^ ]*) \+- ([^ ]*)$")
+    xsec = None
+    eq_lumi = None
+    for output_line in xsec_info_output.splitlines():
+        xsec_re_match = xsec_re.match(output_line)
+        if xsec_re_match:
+            if printDebug: print("Line matching \"xsec_re\": {o}".format(o=output_line))
+            xsec_value = float(xsec_re_match.group(1))
+            xsec_error = float(xsec_re_match.group(2))
+            if (math.fabs(xsec_error/xsec_value) > XSEC_FRACTIONAL_ERROR_TOLERANCE): sys.exit("ERROR: too large an uncertainty in cross-section. Measured: {v} +- {e}, fractional uncertainty = {u}.".format(v=xsec_value, e=xsec_error, u=math.fabs(xsec_error/xsec_value)))
+            if (xsec is None):
+                if printDebug: print("Found xsec: {x} +- {xe}".format(x=xsec_value, xe=xsec_error))
+                xsec = xsec_value
+            else:
+                sys.exit("ERROR: multiple lines seem to contain xsec value.")
+        eq_lumi_re_match = eq_lumi_re.match(output_line)
+        if eq_lumi_re_match:
+            if printDebug: print("Line matching \"eq_lumi_re\": {o}".format(o=output_line))
+            eq_lumi_value = float(eq_lumi_re_match.group(1))
+            eq_lumi_error = float(eq_lumi_re_match.group(2))
+            if (math.fabs(eq_lumi_error/eq_lumi_value) > XSEC_FRACTIONAL_ERROR_TOLERANCE): sys.exit("ERROR: too large an uncertainty in equivalent lumi. Measured: {v} +- {e}, fractional uncertainty = {u}.".format(v=eq_lumi_value, e=eq_lumi_error, u=math.fabs(eq_lumi_error/eq_lumi_value)))
+            if (eq_lumi is None):
+                if printDebug: print("Found eq_lumi: {e} +- {ee}".format(e=eq_lumi_value, ee=eq_lumi_error))
+                eq_lumi = eq_lumi_value
+            else:
+                sys.exit("ERROR: multiple lines seem to contain eq_lumi value.")
+    if ((xsec is None) or (eq_lumi is None)): sys.exit("ERROR: either cross-section or equivalent_lumi not found.")
+    # The script runs over 1 million events. So we expect
+    # cross_section*equivalent_lumi = 1000000
+    # However, someone decided that cross_section would be measured in pb
+    # and equivalent lumi in (1/fb). So we expect
+    # (cross_section/pb)*(equivalent_lumi/(1/fb)) = 1000
+    # UPDATE: this only seems to work for some datasets, so commenting out for now...
+    # cross_check = math.fabs((xsec*eq_lumi/1000.0) - 1.0)
+    # if (cross_check > XSEC_FRACTIONAL_ERROR_TOLERANCE): sys.exit("ERROR: cross-section={x} and equivalent_lumi={l} fail sanity check. Expected their product to be 1000, but found: {p}".format(x=xsec, l=eq_lumi, p=xsec*eq_lumi))
+    # return (xsec, eq_lumi)
+    return xsec
 
-def get_xsec_info_for_dataset(dataset_name):
+def get_xsec_info_for_dataset(dataset_name, identifier, printDebug=True):
+    # Unlike most other functions in tmPyUtils modules, this particular function is not
+    # isolated well enough from other analysis steps
+    # In particular, it requires setting up my git repo for the "official" cross-section tool
+    if printDebug: print("Running over dataset {d} with id={i}".format(d=dataset_name, i=identifier))
+    dataset_nevents = get_number_of_events_in_dataset(dataset_name)
+    if printDebug: print("nevents in dataset: {n}".format(n=dataset_nevents))
     cwd = os.getcwd()
     dataset_mcm_id = get_mcm_prepid_for_dataset(dataset_name)
-    shell_session_handle = subprocess.Popen("env -i bash", stdout=subprocess.PIPE, stdin=subprocess.PIPE, shell=True, executable="/bin/bash")
-    xsec_setup(shell_session_handle)
-    shell_session_handle.terminate()
-    returncode = shell_session_handle.poll()
-    if (returncode is None): print("Something's gone wrong, shell process was supposed to have terminated...")
+    if ("CMSSW_BASE" in os.environ):
+        sys.exit("ERROR: xsec utility requires you to start from a \"fresh\" environment. Currently CMSSW_BASE is set to: {v}".format(v=os.environ["CMSSW_BASE"]))
+    xsec_scram_arch = os.environ["XSEC_SCRAM_ARCH"]
+    path_xsec_cmssw = os.environ["PATH_XSEC_CMSSW"]
+    path_xsec_scripts = os.environ["PATH_XSEC_SCRIPTS"]
+    xsec_info_output = str(subprocess.check_output("cd {c}/Utilities/calculateXSectionAndFilterEfficiency && ./tmGetXSecWrapper.sh {m} {i} {sa} {cmssw} {ps}".format(c=cwd, m=dataset_mcm_id, i=identifier, sa=xsec_scram_arch, cmssw=path_xsec_cmssw, ps=path_xsec_scripts), shell=True, stderr=subprocess.STDOUT, executable="/bin/bash", universal_newlines=True))
+    xsec = parse_xsec_info_output(xsec_info_output)
+    if printDebug: print("xsec: {x}".format(x=xsec))
+    return (xsec, dataset_nevents)
+
+def save_dataset_xsec_info_into_json(dataset_name, identifier, output_path):
+    xsec, nevents = get_xsec_info_for_dataset(dataset_name, identifier)
+    output_dict = {
+        "xsec": xsec,
+        "nevents": nevents
+    }
+    with open(output_path, 'w') as json_output_file_handle:
+        json.dump(output_dict, json_output_file_handle, indent=4, separators=(',', ': '))
+    # json.dump for some reason doesn't save output with a newline at the end
+    with open(output_path, 'a') as json_output_file_handle:
+        json_output_file_handle.write("\n")
